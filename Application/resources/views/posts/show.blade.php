185b08fcf8a2afb8167911ebddf705712ef21ade
Iman Imen <imanimen81@gmail.com>
2021-12-12 14:06:33 +0330

    user permission to edit & delete posts, fixed

--- Application/resources/views/posts/show.blade.php
+++ Application/resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default">Go Back</a>
+    <a href="/posts" class="btn btn-secondary mt-4">Go Back</a>
     <div class="container">
         <div class="mt-4">
             <h1>{{$post->title}}</h1>
@@ -13,14 +13,16 @@
         <small>Written on {{$post->created_at}} by {{$post->user->name}}
             <hr>
             @if(!Auth::guest())
-        <div>
-            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-        </div>
-        <br>
-        {!!Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-left'])!!}
-                {{Form::hidden('_method', 'DELETE')}}
-                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-        {!! Form::close() !!}
+                @if(Auth::user()->id == $post->user_id)
+                    <div>
+                        <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+                    </div>
+                    <br>
+                    {!!Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-left'])!!}
+                            {{Form::hidden('_method', 'DELETE')}}
+                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                    {!! Form::close() !!}
+             @endif
         @endif
     </div>
 @endsection
